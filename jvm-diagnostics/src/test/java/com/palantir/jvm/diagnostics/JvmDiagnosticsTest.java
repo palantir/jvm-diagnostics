6b73dae57d8d678a7ad5a930abcdf3e3d32ea1de
Alexis Le Dantec <aledantec@palantir.com>
2025-12-12 16:52:26 +0100

    Revert "Actually suppress the error-prone"
    This reverts commit 9135863a18f020e90e3129dc9f5485fa58b485d8.

--- jvm-diagnostics/src/test/java/com/palantir/jvm/diagnostics/JvmDiagnosticsTest.java
+++ jvm-diagnostics/src/test/java/com/palantir/jvm/diagnostics/JvmDiagnosticsTest.java
@@ -26,6 +26,7 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
@@ -159,6 +160,8 @@ class JvmDiagnosticsTest {
             private volatile boolean done = false;
             private final Thread thread;
 
+            private final AtomicLong counter = new AtomicLong(0);
+
             private BusyWaiter(VirtualThreadFactory factory) {
                 this.thread = factory.start(this);
             }
@@ -169,11 +172,9 @@ class JvmDiagnosticsTest {
             }
 
             @Override
-            @SuppressWarnings("StrictUnusedVariable")
             public void run() {
-                int counter = 0;
                 while (!done) {
-                    counter++;
+                    counter.incrementAndGet();
                 }
             }
         }
